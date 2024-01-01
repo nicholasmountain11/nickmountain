df8c4d9fa5b9d7e56b8e05ac8ac28c11f4ddbdca
nicholasmountain11 <nmountain@unc.edu>
2024-01-01 12:11:33 -0500

    change project cards to have no images

--- nick-app/src/app/projects/projects.component.ts
+++ nick-app/src/app/projects/projects.component.ts
@@ -14,14 +14,14 @@ export class ProjectsComponent {
         projectType: 'Class Project',
         description: 'Full stack web application using Angular, FastAPI, and SQLAlchemy for the CSXL Equipment Checkout system',
         link: 'https://github.com/comp423-23f/csxl-final-team-d9',
-        image: 'assets/csxl.png'
+        image: 'assets/csxl-logo.png'
       },
       {
         name: 'Spotify Retro Playlist Generator',
         projectType: 'Hackathon',
         description: 'Uses Python to access the Spotify web API to get data about the most popular songs of the 2000s and create an Ultimate 2000s playlist.',
         link: 'https://github.com/nicholasmountain11/spotify',
-        image: 'assets/spotify.png'
+        image: 'assets/spotify-logo.png'
       }
     ]
 }
