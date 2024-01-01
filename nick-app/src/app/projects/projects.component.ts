7a91d57147732e6b6dfffdacc87ed3e7b219cead
nicholasmountain11 <nmountain@unc.edu>
2023-12-31 19:36:31 -0500

    write spotify project card

--- nick-app/src/app/projects/projects.component.ts
+++ nick-app/src/app/projects/projects.component.ts
@@ -10,11 +10,18 @@ export class ProjectsComponent {
   
     public projects: Project[] = [
       {
-      name: 'CSXL Equipment Checkout',
-      projectType: 'Class Project',
-      description: 'Full stack web application using Angular, FastAPI, and SQLAlchemy for the CSXL Equipment Checkout system',
-      link: 'https://github.com/comp423-23f/csxl-final-team-d9',
-      image: 'assets/csxl.png'
+        name: 'CSXL Equipment Checkout',
+        projectType: 'Class Project',
+        description: 'Full stack web application using Angular, FastAPI, and SQLAlchemy for the CSXL Equipment Checkout system',
+        link: 'https://github.com/comp423-23f/csxl-final-team-d9',
+        image: 'assets/csxl.png'
       },
+      {
+        name: 'Spotify Retro Playlist Generator',
+        projectType: 'Hackathon',
+        description: 'Uses Python to access the Spotify web API to get data about the most popular songs of the 2000s and create an Ultimate 2000s playlist.',
+        link: 'https://github.com/nicholasmountain11/spotify',
+        image: 'assets/spotify.png'
+      }
     ]
 }
