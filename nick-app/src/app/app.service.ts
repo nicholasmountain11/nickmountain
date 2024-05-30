f9175d96eea27d55191934c5addb31a8b3be865c
nicholasmountain11 <nmountain@unc.edu>
2024-05-29 20:54:44 -0400

    set up employee data model and app service

---
+++ nick-app/src/app/app.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Employee } from './models/employee.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  private urlStr: string;
+
+  constructor(private http: HttpClient) { 
+    this.urlStr = "'http://localhost:8080/employees"
+  }
+
+  
+}
