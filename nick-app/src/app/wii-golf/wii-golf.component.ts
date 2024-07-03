1cad17790651a2c1e22ecd5c8394b80fd5e4d882
nicholasmountain11 <nmountain@unc.edu>
2024-07-02 20:42:41 -0400

    add docstrings and error handling to wii-golf.component.ts

--- nick-app/src/app/wii-golf/wii-golf.component.ts
+++ nick-app/src/app/wii-golf/wii-golf.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AppService } from '../app.service';
 import { Player } from '../models/player.model';
-import { Round } from '../models/round.model';
-import { Observable } from 'rxjs';
-import { PlayerWidget } from '../widgets/player/player.widget';
-import { RoundWidget } from '../widgets/round/round.widget';
 import { AddRound } from '../models/add-round.model';
 import { TopRoundReturnModel } from '../models/top-round-return.model';
 
@@ -15,9 +11,9 @@ import { TopRoundReturnModel } from '../models/top-round-return.model';
 })
 export class WiiGolfComponent {
 
-  players$: Player[] | undefined;
+  players$: Player[] | undefined; // list of all Players in the database
 
-  top_rounds$: TopRoundReturnModel[] | undefined;
+  top_rounds$: TopRoundReturnModel[] | undefined; // list of lowest scoring Rounds in the database
 
 
   constructor(private appService: AppService) {
@@ -30,35 +26,48 @@ export class WiiGolfComponent {
       .subscribe((rounds) => this.top_rounds$ = rounds);
   }
 
+  /**
+   * calls getPlayers() service method to refresh the players$ field
+   */
   refreshPlayers() {
     this.appService
       .getPlayers()
       .subscribe((players) => (this.players$ = players));
   }
 
+  /**
+ * calls getTopRounds() service method to refresh the top_rounds$ field
+ */
   refreshTopRounds() {
     this.appService
       .getTopRounds(10)
       .subscribe((rounds) => this.top_rounds$ = rounds);
   }
 
+  /**
+   * calls registerPlayer() service method to add Player to the database
+   * @param player Player to be added to the database
+   */
   registerPlayer(player: Player) {
     this.appService.registerPlayer(player).subscribe({
       next: () => {
         this.refreshPlayers();
-      }
+      },
+      error: (error) => console.log(error)
     });
   }
 
+  /**
+ * calls addRound() service method to add Player to the database
+ * @param round Round to be added to the database
+ */
   addRound(round: AddRound) {
     this.appService.addRound(round).subscribe({
       next: () => {
         this.refreshPlayers();
         this.refreshTopRounds();
-      }
+      },
+      error: (error) => console.log(error)
     })
   }
-
-
-
 }
