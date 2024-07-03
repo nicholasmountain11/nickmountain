7c7d194f031de926dc63fb8acc27949e3bbc9e12
nicholasmountain11 <nmountain@unc.edu>
2024-07-02 20:36:31 -0400

    add service method docstrings

--- nick-app/src/app/app.service.ts
+++ nick-app/src/app/app.service.ts
@@ -18,22 +18,46 @@ export class AppService {
     this.urlStr = "https://wii-golf-service.yellowbush-f41b9f35.eastus.azurecontainerapps.io"
   }
 
+  /**
+ * GET a list of all Players in the database
+ * @returns Observable<Player[]>
+ */
   public getPlayers(): Observable<Player[]> {
     return this.http.get<Player[]>(`${this.urlStr}/player`)
   }
 
+  /**
+   * return a list of all the Holes for a given Round
+   * @param roundId The ID of the Round being requested
+   * @returns Observable<Hole[]>
+   */
   public getHolesByRound(roundId: number): Observable<Hole[]> {
     return this.http.get<Hole[]>(`${this.urlStr}/getHolesByRound/${roundId}`)
   }
 
+  /**
+   * add a Player to the database
+   * @param player The Player to be added to the database
+   * @returns the Player that was added
+   */
   public registerPlayer(player: Player) {
     return this.http.post<Player>(`${this.urlStr}/player`, player);
   }
 
+  /**
+   * Add a Round to a given Player
+   * @param round AddRound object containing the Round to be added and the ID of the player to add the Round to
+   * @returns the Round that was added to the database
+   */
   public addRound(round: AddRound) {
     return this.http.post<Hole[]>(`${this.urlStr}/addRoundByPlayer/${round.player_id}`, round.holes);
   }
 
+  /**
+   * GETs the $$roundAmount$$ lowest scoring Rounds in the database
+   * @param roundAmount The number of Rounds to GET
+   * @returns Observable<TopRoundReturnModel[]>
+   */
   public getTopRounds(roundAmount: number): Observable<TopRoundReturnModel[]> {
     return this.http.get<TopRoundReturnModel[]>(`${this.urlStr}/getLowestScoringRounds/${roundAmount}`);
   }
