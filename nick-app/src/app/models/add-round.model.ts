6382d18518a9f8234e3d3f894d8203752c16d865
nicholasmountain11 <nmountain@unc.edu>
2024-08-30 17:04:52 -0400

    document data models

--- nick-app/src/app/models/add-round.model.ts
+++ nick-app/src/app/models/add-round.model.ts
@@ -1,12 +1,15 @@
-/**
- * Model used to add a Round to the database
- * Contains a list of associated Holes, and the player_id of the Player
- * who played the Round. 
- */
-
 import { Hole } from "./hole.model";
-
+/**
+ * Model used to add a Round to the database.
+ * Contains a list of associated Holes and the player_id of the Player
+ * who played the Round.
+ * 
+ * @property {Hole[]} holes - An array of Hole objects representing each hole played in the Round.
+ * @property {number} player_id - The unique identifier of the Player who played the Round.
+ */
 export interface AddRound {
+    /** An array of Hole objects representing each hole played in the Round */
     holes: Hole[];
+    /** ID of the Player who played the Round */
     player_id: number;
 }
--- nick-app/src/app/models/project.model.ts
+++ nick-app/src/app/models/project.model.ts
@@ -1,11 +1,22 @@
 /**
  * Project model provides necessary information for the description of a project.
+ * 
+ * @property {string} name - The name of the Project
+ * @property {string} projectType - The type of the Project, ex) personal project or class project.
+ * @property {string} description - A short description of the projects purpose and implimentation.
+ * @property {string} link - A link to the gitHub page of the Project.
+ * @property {Set<string>} technologies - A set of the technologies used to build the Project.
  */
 
 export interface Project {
+    /** The name of the Project */
     name: string;
+    /** The type of the Project, ex) personal project or class project */
     projectType: string;
+    /** A short description of the projects purpose and implimentation */
     description: string;
+    /** A link to the gitHub page of the Project */
     link: string;
+    /** A set of the technologies used to build the Project */
     technologies: Set<string>;
 }
