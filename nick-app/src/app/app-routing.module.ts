438dedaba6b36d610e793e925428d6f9e9df7f0c
nicholasmountain11 <nmountain@unc.edu>
2024-09-02 11:31:32 -0400

    remove classes component to fix errors

--- nick-app/src/app/app-routing.module.ts
+++ nick-app/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { ClassesComponent } from './classes/classes.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
 import { WiiGolfComponent } from './wii-golf/wii-golf.component';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
@@ -12,7 +11,6 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about-me', component: AboutMeComponent },
   { path: 'projects', component: ProjectsComponent },
-  { path: 'classes', component: ClassesComponent },
   { path: 'work-experience', component: WorkExperienceComponent },
   { path: 'wii-golf', component: WiiGolfComponent }
 ];
